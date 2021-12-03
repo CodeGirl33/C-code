9af8d8b9038fef489134ab13b7610853cb5f6071
giuliabryson <giuliabryson@protonmail.com>
2021-12-03 21:03:09 +0000

    take2

---
+++ scorearray0.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+const int TOTAL = 3;
+float average(int length, int scores[]);
+
+int main(void)
+{
+	/*int total = get_int("Total umber of scores: "); */
+	int scores[TOTAL];
+	for (i = 0; i < TOTAL; i++)
+		{
+			scores[i] = get_int("Enter score: ");
+			/*scanf("%d\n", &scores); */
+		}
+	printf("Average: %f\", TOTAL / (float) sum );
+
+}
+float average(int length, int scores[])
+{
+	int sum = 0;
+	
+
+}
